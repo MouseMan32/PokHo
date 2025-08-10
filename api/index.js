690dad1ab5b3fafc0f6dd99b92cd3ee1f85cbd44
MouseMan32 <94012511+MouseMan32@users.noreply.github.com>
2025-08-09 22:45:47 -0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -15,6 +15,19 @@ import {
 } from "./parsers/gen6_xy.js";
 import { readMeta, writeMeta } from "./store/meta.js";
 
+import cors from "cors";
+
+// allow local dev ports only
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+  ],
+  credentials: false
+}));
+
 const app = express();
 app.use(express.json());
 
