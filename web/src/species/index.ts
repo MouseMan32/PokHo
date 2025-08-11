cb6acccd65340e27012230f31417ffbd8345f126
MouseMan32 <94012511+MouseMan32@users.noreply.github.com>
2025-08-11 09:42:27 -0600

    Update index.ts

--- web/src/species/index.ts
+++ web/src/species/index.ts
@@ -1,1 +1,7 @@
+export { gen1 } from "./gen1";
+export { gen2 } from "./gen2";
+export { gen3 } from "./gen3";
+export { gen4 } from "./gen4";
+export { gen5 } from "./gen5";
+export { gen6 } from "./gen6";
 
